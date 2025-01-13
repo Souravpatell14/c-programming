a1de906933c17828ec520b9f5671a11b4c23c86f
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-13 21:32:45 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -150,26 +150,6 @@ int main()
 
 
 (8) odd number 
-
-#include <stdio.h>
-
-int main()
-{
-    int n;
-    printf("enter no of row");
-    scanf ("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=2*n-1;j=j+2){
-            printf("%d ",j);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-
-
-(9) odd number 
 #include <stdio.h>
 
 int main()
