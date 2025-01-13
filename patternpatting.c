561960fca02875a160a57bed0e7d4f8caf10f89f
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-13 23:51:44 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -292,3 +292,27 @@ int main() {
 
     return 0;
 }
+
+(13) floy patting 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter no of row");
+    scanf("%d",&n);
+    
+      int a = 1;
+      for(int i=1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+             printf("%d ",a);
+             a++;
+         }
+         printf("\n");
+         
+    }
+
+    return 0;
+}
