f2fa6a7f1b660b0233c9e51c18802265c0573ff5
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-13 18:55:12 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -160,7 +160,7 @@ int main()
     scanf ("%d",&n);
     for(int i=1;i<=n;i++){
         for(int j=1;j<=2*n-1;j=j+2){
-            printf("%d",j);
+            printf("%d ",j);
         }
         printf("\n");
     }
