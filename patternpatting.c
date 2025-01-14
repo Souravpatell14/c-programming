fd4d882b7ca0c49ab704b3bcbb8a195e86f1370a
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-14 07:24:40 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -340,3 +340,25 @@ int main()
 
     return 0;
 }
+
+(15) 0 and 1 patting.c
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter no of row");
+    scanf("%d",&n);
+
+      for(int i=1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+             if((i+j)%2==0) printf("1 ");
+             else printf("0 ");
+         }
+         printf("\n");
+
+    }
+
+    return 0;
+}
