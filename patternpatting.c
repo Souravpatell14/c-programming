ef3994bdb0a0ff6dec28358b959a0b0b6e9c2430
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-15 23:51:07 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -362,3 +362,28 @@ int main()
 
     return 0;
 }
+
+
+(16) 🌟 star side  *
+                  **
+                 ***
+
+
+#include <stdio.h>
+
+int main()
+{ int n;
+    printf("enter no of line");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+       for(int k=1;k<=i;k++){
+           printf("*");
+       }
+       printf("\n");
+    }
+
+    return 0;
+}
