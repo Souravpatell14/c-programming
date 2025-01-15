f06e63a0b2981cb1c72c710ec9518bd0dce3e07f
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-01-16 00:25:25 +0530

    Update patternpatting.c

--- patternpatting.c
+++ patternpatting.c
@@ -418,3 +418,25 @@ int main() {
 
     return 0;
 }
+
+
+(18) triangle 🔺️ start 
+
+#include <stdio.h>
+
+int main()
+{ int n;
+    printf("enter no of line");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+       for(int k=1;k<=i;k++){
+           printf("* ");
+       }
+       printf("\n");
+    }
+
+    return 0;
+}
