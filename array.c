f1155537966169c2ee93ec32d69d562a0a75db5b
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-02-26 09:27:40 +0530

    Update array.c

--- array.c
+++ array.c
@@ -129,3 +129,24 @@ int main()
     printf("%d",totaltriplets);
     return 0;
 }
+
+
+
+(8) maximum number.c
+
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int arr[7] = {-10,-4,-200,-80,-19,-5,-12};
+    int max = INT_MIN;
+  for(int i=0;i<=6;i++){
+   if (max<arr[i]){
+          max = arr[i];
+   }
+  }
+  printf("%d",max);
+
+    return 0;
+}
