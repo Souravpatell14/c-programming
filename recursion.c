9797e60a05b1c2415f8565e83e1b35e97632aa67
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-02-27 06:01:41 +0530

    Update

---
+++ recursion.c
@@ -0,0 +1,19 @@
+(1)  factorial recursion 
+
+#include <stdio.h>
+
+int factorial(int n){
+    if(n==0 || n==1) return 1;
+    return n*factorial(n-1);
+}
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    int fact = factorial(n);
+    printf("%d",fact);
+
+    return 0;
+}
