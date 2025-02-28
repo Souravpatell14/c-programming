a2ab6848547fa96f0e41ad85913acb83df7183b6
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-02-28 19:14:51 +0530

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -142,3 +142,24 @@ int main()
     return 0;
 }
 
+
+(6) fibo recursion 
+
+
+#include <stdio.h>
+
+int fibo(int n){
+    if(n==1 || n==2) return 1;
+    return fibo (n-1) + fibo (n-2);
+}
+
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    
+    printf("%d",fibo(n));
+
+    return 0;
+
