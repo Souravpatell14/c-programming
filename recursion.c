574f30058f14b805b84b22b418c05d2f4b3199ce
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-02-28 09:04:21 +0530

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -115,3 +115,30 @@ int main()
     return 0;
 }
 
+
+
+(5)  power recursion 
+
+
+#include <stdio.h>
+
+int power(int a, int b){
+    if(b==0) return 1;
+    int recAns = a*power(a,b-1);
+    return recAns;
+}
+
+int main()
+{
+    int a;
+    printf("enter a base");
+    scanf("%d",&a);
+    int b;
+    printf("enter a power");
+    scanf("%d",&b);
+    int p = power(a,b);
+    printf("%d raised to the power %d is %d",a,b,p);
+
+    return 0;
+}
+
