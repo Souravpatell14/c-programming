77448bb79f0610bc3d958b539e9fc0624f10fcb0
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-01 00:50:22 +0530

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -186,3 +186,34 @@ int main()
 
     return 0;
 }
+
+
+(8) powerlog 
+
+#include <stdio.h>
+
+int powerlog(int a, int b){
+    if(b==0) return 1;
+    int x = powerlog(a,b/2);
+    
+    if (b%2==0)
+        return x*x;
+    else 
+        return x*x*a;
+}
+
+int main()
+{
+    int a;
+    printf("enter a base");
+    scanf("%d",&a);
+    
+    int b;
+    printf("enter a power");
+    scanf("%d",&b);
+    
+    int p= powerlog(a,b);
+    printf("%d raised to the power %d is %d",a,b,p);
+
+    return 0;
+}
