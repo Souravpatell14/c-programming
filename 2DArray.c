eb4b8749d8ecbdf31591679f4594f09fe9426796
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-01 22:56:02 +0530

    Update 2DArray.c

--- 2DArray.c
+++ 2DArray.c
@@ -13,3 +13,29 @@ int main()
     }
     return 0;
 }
+
+
+(2) user_input 
+
+#include <stdio.h>
+
+int main() {
+    int arr[3][3];
+
+    // Input elements
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);  // Corrected format specifier
+        }
+    }
+
+    // Output elements
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");  // New line after each row
+    }
+
+    return 0;
+}
