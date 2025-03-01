1f134d9c6846bfb39d362bb46be8ec98c7feab91
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-01 21:50:16 +0530

    Create strings.c

---
+++ strings.c
@@ -0,0 +1,13 @@
+(1) stringbasic
+
+#include <stdio.h>
+
+int main()
+{
+    char arr[]="welcome to strings\0";
+    int i=0;
+    while(arr[i]!='\0'){
+    printf("%c",arr[i]);
+    i++;
+}
+    return 0;
