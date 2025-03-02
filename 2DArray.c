086a4072ff7f7fb24db067462b09bd2bc559e17a
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-02 23:59:24 +0530

    Update 2DArray.c

--- 2DArray.c
+++ 2DArray.c
@@ -146,3 +146,42 @@ int main() {
 
     return 0;
 }
+
+
+(6) transpose in place 
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of row/column");
+    scanf("%d",&n);
+    printf("Enter the all elements\n");
+    
+    int arr[n][n]; //n*n total elements
+    //input 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           scanf("%d",&arr[i][j]);
+        }
+    }
+    //transpose 
+     for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int tamp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = tamp;
+            
+        }
+     }
+     //output 
+          for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
