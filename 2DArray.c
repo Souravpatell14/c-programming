450c9a63faee9d62f460f3f2f100b80103aeb1d4
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-04 01:02:06 +0530

    Update 2DArray.c

--- 2DArray.c
+++ 2DArray.c
@@ -297,3 +297,64 @@ int main()
 }
 
 
+(9) multiple 2 matrix input user's 
+
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    printf("enter row of 1st matrix:");
+    scanf("%d",&m);
+    int n;
+    printf("enter column of 1st matrix:");
+    scanf("%d",&n);
+    
+    int a[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int p;
+    printf("enter row of 2nd matrix:");
+    scanf("%d",&p);
+    int q;
+    printf("enter column of 2nd matrix:");
+    scanf("%d",&q);
+    int b[p][q];
+    
+    for(int i=0;i<p;i++){
+        for(int j=0;j<q;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    // check 
+    if(n!=p){
+        printf("the matrix cannot multipy");
+    }
+    else{
+        //multiplication 
+      int res[m][q];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<q;j++){
+                res[i][j]=0;
+                 // I row of a and j column of b
+                for(int k=0;k<n;k++){
+                    res[i][j]+=a[i][k]*b[k][j];
+                }
+           }
+    }
+    // print
+    printf("The resultant matrix is \n");
+    for(int i=0;i<m;i++){
+        for(int j=0;j<q;j++){
+            printf("%d ",res[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+    return 0;
+}
+
